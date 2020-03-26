ca3441666efb69b58d4094ea5a33848d6b5daa8b
yanhao98 <37316281+yanhao98@users.noreply.github.com>
2020-03-26 22:47:34 +0800

    FAB 修改

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -11,15 +11,15 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Wangyi News'),
       ),
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
         children: <Widget>[
           FloatingActionButton(
               child: Icon(Icons.refresh),
               onPressed: () {
                 _bloc.add(RefreshNews());
               }),
-          SizedBox(width: 16),
+          SizedBox(height: 16),
           FloatingActionButton(
               child: Icon(Icons.navigate_next),
               onPressed: () {
